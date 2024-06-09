088bc67191e39b682e884bdcbcc46b41799f85c1
Rolando Gonzalez <rolando.fgrgmail.com>
2024-06-09 13:01:36 -0400

    deleted iso from layers

--- src/components/main/context/maps/index.tsx
+++ src/components/main/context/maps/index.tsx
@@ -1,16 +1,13 @@
 // App imports
 import { MapboxProvider } from './mapbox';
-import { LayersProvider } from './layers';
 import { ClustersProvider } from './clusters';
 
 export const MapsProvider = ({children}: any) => {
   return (
     <MapboxProvider>
-    <LayersProvider>
     <ClustersProvider>
       {children}
     </ClustersProvider>
-    </LayersProvider>
     </MapboxProvider>
   )
 }
--- src/components/main/maps/map/clusters/iso/index.tsx
+++ src/components/main/maps/map/clusters/iso/index.tsx
@@ -1,11 +1,39 @@
 // Context imports
-import { useIsoLayer } from '../../../../context/maps/layers/iso';
+import { useIsoPolygonApi } from '../../../../context/api/isoPolygon';
 
 // Third party imports
 import { Source, Layer } from 'react-map-gl';
 
 export const IsoPolygon = () => {
-	const { isoLayer, isoSource } = useIsoLayer();
+	const { isoPolygonData } = useIsoPolygonApi();
+
+	const isoLayer: any = {
+	    id: 'isolayer',
+	    type: 'fill',
+	    source: 'isoSource',
+	    paint: {
+	      'fill-color': 'rgb(222, 112, 112)',
+	      'fill-opacity': 0.2,
+	    },
+	  };
+
+	const isoSource: any = {
+	    type: 'geojson',
+	    data: {
+	      type: 'FeatureCollection',
+	      features: [
+	        {
+	          type: 'Feature',
+	          geometry: {
+	            type: 'Polygon',
+	            coordinates: isoPolygonData ? 
+	            isoPolygonData.features[0].geometry.coordinates
+	            : [],
+	          },
+	        },
+	      ],
+	    },
+	  };
 	
 	return (
 		<Source id="isoSource" {...isoSource}>
