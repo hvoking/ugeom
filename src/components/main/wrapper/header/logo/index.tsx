2cd4fff2c7d5bcbe093f0859bfb651bdfe168216
hvoking <gus@ugeom.com>
2024-08-19 16:13:07 -0400

    backup 19-08-24

--- src/components/main/wrapper/header/logo/index.tsx
+++ src/components/main/wrapper/header/logo/index.tsx
@@ -9,9 +9,7 @@ export const Logo = () => {
 				src="static/logos/white.svg" 
 				alt="header-logo"
 			/>
-			<div>
-				<div className="logo-name">Urban Geometries</div>
-			</div>
+			<div className="logo-name">URBAN GEOMETRIES</div>
 		</div>
 	)
 }
