82cd2bdfe1ff651bc266dade77f4a6e3eaf5ab2b
hvoking <gus@ugeom.com>
2024-10-05 01:31:22 -0400

    backup 10-05-24

--- src/components/main/wrapper/header/logo/index.tsx
+++ src/components/main/wrapper/header/logo/index.tsx
@@ -4,7 +4,11 @@ import './styles.scss';
 export const Logo = () => {
 	return (
 		<div className="logo-wrapper">
-			ugeom
+			<img 
+				src={process.env.PUBLIC_URL + "/static/logos/logo.svg"} 
+				alt="logo"
+				height="45px"
+			/>
 		</div>
 	)
 }
